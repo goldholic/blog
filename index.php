085e6f225555586439c0bf44237ce6b0f9cafd34
gold <carlosholic19@gmail.com>
2018-02-23 13:31:41 +0100

    empezando

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Page Title</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="cabezera">
+    
+    
+</div>
+<div class="cuerpo">
+    
+    
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+<?php
+
+?>
