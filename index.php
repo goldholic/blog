4ad07a8c97cc5039fb5f1c18dcf7829f018d7767
gold <carlosholic19@gmail.com>
2018-02-26 14:02:40 +0100

    cabecera y casi pie

--- index.php
+++ index.php
@@ -7,13 +7,32 @@
 </head>
 
 <body>
-<div class="cabezera">
+<div class="cabecera">
+    <div class="contenidocabecera">
+        <a href="http://www.ajeclm.com/" target="blanck" class="logo"></a>
+        <a href="index.php""><h1>Blog del Programa TIC</h1></a>
     
+  </div>
     
+ </div> 
+<div class="fondo">
+    <div class="contenido">
+    
+    </div> 
+    
+<div class="pie">
+    <div class="contenidopie">
+        <a  href="https://www.jccm.es/"target="blank"><img class="junta" src="imagenes/comunes/junta.jpg" alt="logo de la jccm" title="logo de la jccm"/>
+        </a>
+        <a  href="https://www.ajeclm.com/programa-tic/"target="blank"><img class="programatic" src="imagenes/comunes/programatic.jpg" alt="programatic" title="programatic"/>
+        </a>
+        <a  href="http://www.ajeclm.com/"target="blank"><img class="aje" src="imagenes/comunes/aje.jpg" alt="logo de aje" title="logo de aje"/>
+        </a>
+         <a  href="http://ec.europa.eu/esf/home.jsp?langId=es"target="blank"><img class="europeo" src="imagenes/comunes/europeo.jpg" alt="logo del fondo social europeo" title="logo del fondo social europeo"/>
+        </a>
+        
+   </div>
 </div>
-<div class="cuerpo">
-    
-    
 </div>
 
 </body>
